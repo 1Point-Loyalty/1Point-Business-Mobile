922ec674bd0f6a761aac7a75f0d9cf40ee802bdd
Saif Abuosba <48367948+SaifA21@users.noreply.github.com>
2025-01-26 13:17:10 -0500

    [OP-113] - QR Scanning Implemented (#7)
    * Update QRScan.tsx
    
    * Update QRScan.tsx
    
    * Loading State Implemented

--- app/(auth)/(tabs)/QRScan.tsx
+++ app/(auth)/(tabs)/QRScan.tsx
@@ -1,124 +1,243 @@
-import Ionicons from "@expo/vector-icons/Ionicons";
-import { StyleSheet, Image, Platform } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, Button, StyleSheet } from "react-native";
+import { CameraView, useCameraPermissions } from "expo-camera";
+import auth from "@react-native-firebase/auth";
+import QRInfoLoadingState from "@/components/loadingState/QRInfoLoadingState";
 
-import { Collapsible } from "@/components/Collapsible";
-import { ExternalLink } from "@/components/ExternalLink";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+const QRScan = () => {
+  const [permission, requestPermission] = useCameraPermissions();
+  const [showQRCode, setShowQRCode] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-export default function TabTwoScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
-      headerImage={
-        <Ionicons size={310} name="code-slash" style={styles.headerImage} />
+  interface UserInfo {
+    createdAt: string;
+    currentPoints: number;
+    email: string;
+    firstName: string;
+    id: string;
+    ipAddress: string;
+    isAdmin: number;
+    isBO: number;
+    lastName: string;
+    merchantID: string | null;
+    phoneNumber: string;
+    status: "ACTIVE" | "INACTIVE";
+    updatedAt: string;
+  }
+
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////
+
+  const handleQRCodeDetected = async (code: string) => {
+    setShowQRCode(true);
+    setIsLoading(true);
+
+    console.log("QR Code detected: ", code);
+
+    // decrypt the qr code
+    const user = auth().currentUser;
+    const token = await user?.getIdToken(); // Retrieve the token from storage
+
+    if (!token) {
+      alert("Error, No authentication token found");
+      return;
+    }
+
+    const response = await fetch(
+      "https://admin.1-point.ca/api/decryptUserDetails",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ encryptedQRCode: code }),
       }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">QR Scanning</ThemedText>
-      </ThemedView>
-      <ThemedText>
-        This app includes example code to help you get started.
-      </ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          and{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{" "}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the
-          web version, press <ThemedText type="defaultSemiBold">w</ThemedText>{" "}
-          in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the{" "}
-          <ThemedText type="defaultSemiBold">@2x</ThemedText> and{" "}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to
-          provide files for different screen densities
-        </ThemedText>
-        <Image
-          source={require("@/assets/images/react-logo.png")}
-          style={{ alignSelf: "center" }}
+    );
+
+    if (!response.ok) {
+      alert("Error, Unable to decrypt the QR code");
+      return;
+    } else {
+      console.log(response);
+      const data = await response.json();
+      console.log(data);
+
+      setUserInfo(data[0]);
+    }
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+  };
+
+  ////////////////////////////////////////////////////////////////////////////////////////
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.permissionContainer}>
+        <Text style={styles.permissionMessage}>
+          We need your permission to use the camera
+        </Text>
+        <Button
+          color={"black"}
+          onPress={requestPermission}
+          title="Grant Permission"
         />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText>{" "}
-          to see how to load{" "}
-          <ThemedText style={{ fontFamily: "SpaceMono" }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{" "}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook
-          lets you inspect what the user's current color scheme is, and so you
-          can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{" "}
-          <ThemedText type="defaultSemiBold">
-            components/HelloWave.tsx
-          </ThemedText>{" "}
-          component uses the powerful{" "}
-          <ThemedText type="defaultSemiBold">
-            react-native-reanimated
-          </ThemedText>{" "}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The{" "}
-              <ThemedText type="defaultSemiBold">
-                components/ParallaxScrollView.tsx
-              </ThemedText>{" "}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
+      </View>
+    );
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  const UserDetails: React.FC<{ userInfo: UserInfo }> = ({ userInfo }) => {
+    return (
+      <>
+        <Text style={styles.title}>Customer Information</Text>
+        <Text style={styles.qrCode}>Name: {userInfo.firstName}</Text>
+        <Text style={styles.qrCode}>
+          Available Points: {userInfo.currentPoints}
+        </Text>
+        <Text style={styles.qrCode}>
+          Available to Redeem:{" "}
+          {"$" +
+            // find out how many points can be redeemed
+            // every 1000 points can be redeemed for $10
+            Math.floor(userInfo.currentPoints / 1000) * 10}
+        </Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.actionButton}>
+            <Text style={styles.buttonText}>Award Points</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity disabled={true} style={styles.actionButton}>
+            <Text style={styles.buttonText}>Redeem Points</Text>
+          </TouchableOpacity>
+        </View>
+      </>
+    );
+  };
+  //////////////////////////////////////////////////////////////////////////
+
+  return (
+    <View style={styles.container}>
+      {showQRCode ? (
+        <View style={styles.qrCodeContainer}>
+          {isLoading ? (
+            <QRInfoLoadingState />
+          ) : userInfo ? (
+            <UserDetails userInfo={userInfo} />
+          ) : (
+            <Text>No user information available</Text>
+          )}
+          <TouchableOpacity
+            style={styles.scanAgainButton}
+            onPress={() => {
+              setShowQRCode(false);
+              setUserInfo(null);
+            }}
+          >
+            <Text style={styles.scanAgainText}>Scan Again</Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <CameraView
+          style={styles.camera}
+          facing="back"
+          barcodeScannerSettings={{
+            barcodeTypes: ["qr"],
+          }}
+          onBarcodeScanned={(code) => handleQRCodeDetected(code.data)}
+        ></CameraView>
+      )}
+    </View>
   );
-}
+};
+
+export default QRScan;
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: "#808080",
-    bottom: -90,
-    left: -35,
-    position: "absolute",
+  container: {
+    flex: 1,
+    backgroundColor: "#f8f9fa",
   },
-  titleContainer: {
+  permissionContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f8f9fa",
+  },
+  permissionMessage: {
+    fontSize: 18,
+    textAlign: "center",
+    marginBottom: 20,
+    color: "#495057",
+  },
+  camera: {
+    flex: 1,
+  },
+  qrCodeContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    color: "#212529",
+  },
+  qrCode: {
+    fontSize: 18,
+    color: "#495057",
+    backgroundColor: "#e9ecef",
+    padding: 10,
+    borderRadius: 8,
+    textAlign: "center",
+    marginBottom: 10,
+    width: "100%",
+  },
+  buttonContainer: {
     flexDirection: "row",
-    gap: 8,
+    justifyContent: "space-between",
+    width: "100%",
+    paddingVertical: 20,
+    marginBottom: 20,
+  },
+  actionButton: {
+    backgroundColor: "#f8f9fa",
+    borderColor: "#E95F23",
+    borderWidth: 1,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    width: "45%",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#000",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+  scanAgainButton: {
+    marginTop: 20,
+    backgroundColor: "#E95F23",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    width: "95%",
+  },
+  scanAgainText: {
+    color: "#ffffff",
+    fontSize: 16,
+    fontWeight: "600",
+    textAlign: "center",
   },
 });
