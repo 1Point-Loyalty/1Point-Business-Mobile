c1fb1528603c34a7d0695b3c34a4c5d219a3498e
Aryan Ved <aryanved2002@gmail.com>
2025-02-07 13:04:39 -0500

    Award points UI created

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="editpromotion" options={{ headerShown: false, title: 'lol', headerTitle: 'Edit Promotion',  }} />
+        <Stack.Screen name="awardpoints" options={{ headerShown: false, title: 'lol', headerTitle: 'Award Points',  }} />
       </Stack>
     </ThemeProvider>
   );
